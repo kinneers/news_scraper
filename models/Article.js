4bd94dbc0b7b2b610b452d8d54b1b93d254dbf43
Sarah Kinneer <kinneers@gmail.com>
2019-03-16 17:00:20 -0400

    Article model created

---
+++ models/Article.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+//Save reference to Schema constructor
+var Schema = mongoose.Schema;
+
+//Use Schema constructor to create new UserSchema object
+var ArticleSchema = new Schema({
+    //Ensures that article titles are present in the form of unique strings
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    comment: {
+        //An object that stores a Comment id
+        type: Schema.Types.ObjectId,
+        //links the ObjectId to the Comment model
+        ref: "Comment"
+    }
+});
+
+//Creates the model from the schema using mongoose's model method
+var Article = mongoose.model('Article', ArticleSchema);
+
+//Exports the Article model
+module.exports = Article;
