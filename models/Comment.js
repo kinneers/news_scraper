56e8cf1b450994ddc662c1e68ac2764b4363bd0d
Sarah Kinneer <kinneers@gmail.com>
2019-03-16 17:30:11 -0400

    Comment model and index.js created

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+//Save reference to Schema constructor
+var Schema = mongoose.Schema;
+
+//Create new CommentSchema object with schema constructor
+var CommentSchema = new Schema({
+    //Stores the comment text as a long string
+    text: {
+        type: String,
+        validate: [
+            function(input) {
+                return input.length >= 10;
+            },
+            "Thoughtful comments should be at least 10 characters long."
+        ]
+    }
+});
+
+//Create the model from the schema using mongoose's model method
+var Comment = mongoose.model('Comment', CommentSchema);
+
+//Export the Comment model
+module.exports = Comment;
